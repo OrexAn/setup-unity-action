ac30e7b0e6ba173fb5c1963629f5207627d86a51
Varyvonchyk Aliaksei <avaryvonchyk@exadel.com>
2024-03-07 13:44:11 +0100

    commit

--- .github/actions/setup-unity-action/UnitySetup.sh
+++ .github/actions/setup-unity-action/UnitySetup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+# Set absolute paths for files
+UNITY_TAR="Unity.tar.xz"
+UNITY_SETUP="UnitySetup.sh"
+
 # Check if Unity installer exists
-if [ ! -f "Unity.tar.xz" ]; then
+if [ ! -f "$UNITY_TAR" ]; then
     echo "Unity installer not found. Aborting installation."
     exit 1
 fi
 
 # Extract Unity installer
-tar -xzf Unity.tar.xz || { echo "Failed to extract Unity installer."; exit 1; }
+tar -xzf "$UNITY_TAR" || { echo "Failed to extract Unity installer."; exit 1; }
 
 # Run Unity installer with specified components and options
-./UnitySetup.sh --unattended --install-location=/opt/unity --components=$COMPONENTS || { echo "Failed to install Unity."; exit 1; }
+./"$UNITY_SETUP" --unattended --install-location=/opt/unity --components=$COMPONENTS || { echo "Failed to install Unity."; exit 1; }
 
 # Cleanup
-rm -rf Unity.tar.xz UnitySetup.sh
+rm -rf "$UNITY_TAR" "$UNITY_SETUP"
--- .github/actions/setup-unity-action/install-unity.sh
+++ .github/actions/setup-unity-action/install-unity.sh
@@ -3,9 +3,13 @@ bash
 
      UNITY_VERSION=$1
      COMPONENTS=$2
+	 DOWNLOAD_DIR="${{ github.workspace }}"
 
      # Download and install Unity
-     curl -o UnitySetup.sh https://download.unity3d.com/download_unity/f18e0c1b5784/LinuxEditorInstaller/Unity.tar.xz
+     curl -o "${DOWNLOAD_DIR}/UnitySetup.sh" https://download.unity3d.com/download_unity/f18e0c1b5784/LinuxEditorInstaller/Unity.tar.xz
+	 # Confirm the existence of the downloaded file
+	 ls -l Unity.tar.xz
+	 cd "${DOWNLOAD_DIR}" || exit 1
      tar -xzf Unity.tar.xz
      chmod +x UnitySetup.sh
      ./UnitySetup.sh --unattended --install-location=/opt/unity --components=${COMPONENTS}
