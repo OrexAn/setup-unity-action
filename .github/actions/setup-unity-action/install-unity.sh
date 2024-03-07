2f6bf2e3a4b4ba00d61b07df388ebaac67417d9b
Varyvonchyk Aliaksei <avaryvonchyk@exadel.com>
2024-03-07 13:15:31 +0100

    commit

--- .github/actions/setup-unity-action/install-unity.sh
+++ .github/actions/setup-unity-action/install-unity.sh
@@ -5,7 +5,7 @@ bash
      COMPONENTS=$2
 
      # Download and install Unity
-     curl -o UnitySetup.sh https://download.unity3d.com/download_unity/${UNITY_VERSION}/LinuxEditorInstaller/Unity.tar.xz
+     curl -o UnitySetup.sh https://download.unity3d.com/download_unity/f18e0c1b5784/LinuxEditorInstaller/Unity.tar.xz
      tar -xzf Unity.tar.xz
      chmod +x UnitySetup.sh
      ./UnitySetup.sh --unattended --install-location=/opt/unity --components=${COMPONENTS}
